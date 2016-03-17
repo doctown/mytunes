7d8140cf19cc0600aeb6ecd2cd70ff532458445e
doctown <doctown@yahoo.com>
2016-03-16 17:06:44 -0700

    queues the next song when an "enqueue" event is fired

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -12,10 +12,20 @@ var AppModel = Backbone.Model.extend({
     end up referring to the window. That's just what happens with all JS events. The handlers end up
     getting called from the window (unless we override it, as we do here). */
 
-
     params.library.on('play', function(song) {
       this.set('currentSong', song);
     }, this);
-  }
+
+    //enqueue event happens
+
+    //app listens for the event
+      //if it hears the event, it gets that song from library
+      //it adds song to end of songQueue
+    params.library.on('enqueue', function(song) {
+      this.get('songQueue').add(song);
+    }, this);
+
+  },
+
 
 });
--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,22 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
+  // connect this with the data.js file
+
+  initialize: function() {
+    //is there anything to initialize?
+  },
+
+  default: {},
+
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function() {
+    // Triggering an event here will also trigger the event on the collection
+    this.trigger('enqueue', this);
   }
 
 });
