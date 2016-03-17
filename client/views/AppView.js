c1f66a56139387219803c08e40ce8a2c39858369
David Ogor <doctown@yahoo.com>
2016-03-17 10:53:45 -0700

    Merge branch 'master' of https://github.com/gltr/2016-02-mytunes Add song queue view rendering # Conflicts: #	client/collections/SongQueue.js

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -4,10 +4,8 @@ var AppView = Backbone.View.extend({
   initialize: function(params) {
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
-    // this.playerView.on('ended', function(e) {
-    //   this.model.ended();
-    // });
-    
+    this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
+
     // // change:currentSong - this is Backbone's way of allowing you to filter events to
     // ONLY receive change events for the specific property, 'currentSong'
     this.model.on('change:currentSong', function(model) {
@@ -18,7 +16,8 @@ var AppView = Backbone.View.extend({
   render: function() {
     return this.$el.html([
       this.playerView.$el,
-      this.libraryView.$el
+      this.libraryView.$el,
+      this.songQueueView.$el
     ]);
   }
 
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,30 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.render();
+    // If the collection has a dequeue
+    this.collection.on('change:dequeue', function() {
+      // remove the top el
+    });
+    // If the collection has an enqueue
+    this.collection.on('change:enqueue', function(song) {
+      // add an element to the view
+      this.$el.html('<th>Song Queue</th>').append(new SongQueueEntryView({model: song})).render();
+    });
   },
 
+
   render: function() {
-    return this.$el;
+   // return this.$el;
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
+
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
